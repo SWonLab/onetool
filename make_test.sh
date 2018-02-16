1b54a08a23beb01626f58511875b6a9e610bf62e
yeunjoos <yeunjoo.song@gmail.com>
2018-02-16 15:39:31 -0500

    Update make_test.sh

--- make_test.sh
+++ make_test.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-set -ev
-pwdnow=`${pwd}`
-echo pwdnow
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  cd src
-  echo "in src directory"
-  pwdsrc=`${pwd}`
-  echo pwdsrc
-  #./configure
-  #cd c++
-  #make
-fi
+cd $TRAVIS_BUILD_DIR/src; ./configure; cd c++; make
